f4c23d8cd1c0d1c24ec9bbfac86caf53c537cf46
piro-engineer <hiroki.ne.com@gmail.com>
2023-11-17 16:24:24 +0900

    バリデーション修正

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -13,16 +13,19 @@ class Postcontroller extends Controller
 
     public function store(Request $request) {
         $validated = $request->validate([
-            'title' => 'required|max20',
-            'body' => 'required|max400',
+            'title' => 'required|max:20',
+            'body' => 'required|max:400',
         ]);
 
-        $post = Post::create([
-            'title' => $request->title,
-            'body' => $request->body
-        ]);
+        $validated['user_id'] = auth()->id();
 
         $post = Post::create($validated);
+
         return back()->with('message', '保存しました');
+    }
+
+    public function index(){
+        $posts = Post::all();
+        return view('post.index', compact('posts'));
     }
 }
